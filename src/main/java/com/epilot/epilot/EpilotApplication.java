f4e0684ef9e40843c619d958d2f91ee7fba74f49
HeuristicHiker <connor.dev.johnston@gmail.com>
2024-09-26 10:36:17 -0400

    initial commit

---
+++ src/main/java/com/epilot/epilot/EpilotApplication.java
@@ -0,0 +1,13 @@
+package com.epilot.epilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epilot/epilot/EpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epilot.epilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
